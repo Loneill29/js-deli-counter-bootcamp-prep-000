128595b618f91182a44e561a94018cd3616bbb9d
Linda ONeill <oneill.linda21@gmail.com>
2017-11-15 20:48:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-var katzDeli = [];
-function takeANumber(katzDeli, name) {katzDeli.push (`${name}`)
-return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`)}
+function takeANumber(line, name) {
+  line.push(name)
+  return ('Welcome, ${name}. You are number ${line.length} in line.')
+}
+function nowServing(line) {
+  if (line.length === 0)
+  return ("There is nobody waiting to be served!")
+  else return ("Currently serving ${line.shift()}.")
+}
+function currentLine(line){
+  let katzDeliLine = []
+  if (line.length === 0)
+  return ("The line is currently empty.")
+  else return ("The line is currently: #{katzDeliLine()}")
+}  
 
-
-function nowServing(katzDeli) {let i = 0; while (i < katzDeli.length) {i++};
-if (katzDeli.length === 0) return ("There is nobody waiting to be served!");
-else return (`Currently serving ${katzDeli.shift()}.`);
- }
-var line = [];
- function currentLine(katzDeli) {
-    let i = 0;
-    while (i < katzDeli.length) {
-      line.push(` `+[i+1]+`. `  + katzDeli[i])
-      i++;
-    }
-    if (katzDeli.length === 0) {
-      return "The line is currently empty.";
-    } else
-    return(`The line is currently:` + line);
-  }
